f7cfa83f57277bc514a5593f86082bb68aa49efe
Ngoc Phan <nphan4@yahoo.com>
2018-05-17 18:10:19 -0600

    Add eslintrc and linted

--- db/seeds/test/test.js
+++ db/seeds/test/test.js
@@ -2,8 +2,8 @@ const houses = require('../../../scraped-data/houses-data.json');
 const characters = require('../../../scraped-data/characters-data.json');
 
 houses.forEach(house => {
-  house.members = characters.filter(character => character.HOUSE === house.name)
-})
+  house.members = characters.filter(character => character.HOUSE === house.name);
+});
 
 const createHouse = (knex, house) => {
   return knex('houses').insert({
@@ -14,11 +14,11 @@ const createHouse = (knex, house) => {
     ghost: house.Ghost,
     common_room: house['Common room']
   }, 'id')
-  .then(function(houseId) {
-    let characterPromises = [];
+    .then(function(houseId) {
+      let characterPromises = [];
 
-    house.members.forEach(member => {
-      characterPromises.push(createCharacter(knex, {
+      house.members.forEach(member => {
+        characterPromises.push(createCharacter(knex, {
           name: member.NAME,
           birthday: member.BIRTHDAY || 'NA',
           patronus: member.PATRONUS || 'NA',
@@ -31,29 +31,29 @@ const createHouse = (knex, house) => {
           house: member.HOUSE || 'NA',
           house_id: houseId[0]
         }));
-    })
-    return Promise.all(characterPromises)
-  })
-}
+      });
+      return Promise.all(characterPromises);
+    });
+};
 
 const createCharacter = (knex, character) => {
-  return knex('characters').insert(character)
-}
+  return knex('characters').insert(character);
+};
 
 exports.seed = function(knex, Promise) {
   return knex('characters').del()
     .then(function() {
-      knex('houses').del()
+      knex('houses').del();
     })
     .then(function() {
       let housePromises = [];
 
       houses.forEach(house => {
-        housePromises.push(createHouse(knex, house))
-      })
-      return Promise.all(housePromises)
+        housePromises.push(createHouse(knex, house));
+      });
+      return Promise.all(housePromises);
     })
-  .catch(err => {
-    console.log(err)
-  })
-}
+    .catch(err => {
+      console.log(err);
+    });
+};
